55d146cdcf39d9cbb06bce0ff20893ed733ad0ce
Prabhat Khande <139313451+prabhatkhande@users.noreply.github.com>
2024-06-26 19:54:43 +0530

    Update calculator.sh

--- shell_scripts/calculator.sh
+++ shell_scripts/calculator.sh
@@ -3,64 +3,10 @@
 #Simple Calculator Program
 
 function addition() {
-	echo -n "Enter first number : "
-	read num1
-	echo -n "Enter second number : "
-	read num2
+	num1=$1
+ 	num2=$2
 	result=$((num1 + num2))	
 	echo "Sum of two numbers is : $result"
 
 }
-
-function subtract() {
-	echo -n "Enter first number : "
-        read num1
-        echo -n "Enter second number : "
-        read num2
-        result=$((num1 - num2))
-        echo "Subtraction of two numbers is : $result"
-}
-
-function multiply() {
-        echo -n "Enter first number : "
-        read num1
-        echo -n "Enter second number : "
-        read num2
-        result=$((num1 * num2))
-        echo "Multiplication of two numbers is : $result"
-}
-
-function division() {
-        echo -n "Enter first number : "
-        read num1
-        echo -n "Enter second number : "
-        read num2
-        result=$((num1 / num2))
-        echo "Division of two numbers is : $result"
-}
-
-function percent() {
-        echo -n "Enter obtained marks : "
-        read num1
-        echo -n "Enter total marks : "
-        read num2
-        result=$((100*num1 / num2))
-        echo "Percentage of given number is : $result"
-}
-
-
-echo "Which operation do you want to perform -"
-	echo "a = For Addition"
-        echo "b = For Subtraction"
-        echo "c = For Multiplication"
-        echo "d = For Division"
-        echo "e = For Percentage"
-echo -n "Enter your choice : "
-read choice
-case $choice in
-	a)addition;;
-	b)subtract;;
-	c)multiply;;
-	d)division;;
-	e)percent;;
-esac
+addition $1 $2
