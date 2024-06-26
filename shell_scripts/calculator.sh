53e851d2c42b6825c3ff793d92f793a919c92aed
Prabhat Khande <prabhatkhande786@gmail.com>
2024-06-26 14:56:46 +0530

    Changes in calculator.sh

--- shell_scripts/calculator.sh
+++ shell_scripts/calculator.sh
@@ -4,47 +4,47 @@
 
 function addition() {
 	echo -n "Enter first number : "
-	read num1
+	read $2
 	echo -n "Enter second number : "
-	read num2
-	result=$((num1 + num2))	
+	read $3
+	result=$(($2 + $3))	
 	echo "Sum of two numbers is : $result"
 
 }
 
 function subtract() {
 	echo -n "Enter first number : "
-        read num1
+        read $2
         echo -n "Enter second number : "
-        read num2
-        result=$((num1 - num2))
+        read $3
+        result=$(($2 - $3))
         echo "Subtraction of two numbers is : $result"
 }
 
 function multiply() {
         echo -n "Enter first number : "
-        read num1
+        read $2
         echo -n "Enter second number : "
-        read num2
-        result=$((num1 * num2))
+        read $3
+        result=$(($2 * $3))
         echo "Multiplication of two numbers is : $result"
 }
 
 function division() {
         echo -n "Enter first number : "
-        read num1
+        read $2
         echo -n "Enter second number : "
-        read num2
-        result=$((num1 / num2))
+        read $3
+        result=$(($2 / $3))
         echo "Division of two numbers is : $result"
 }
 
 function percent() {
         echo -n "Enter obtained marks : "
-        read num1
+        read $2
         echo -n "Enter total marks : "
-        read num2
-        result=$((100*num1 / num2))
+        read $3
+        result=$((100*$2 / $3))
         echo "Percentage of given number is : $result"
 }
 
@@ -56,8 +56,8 @@ echo "Which operation do you want to perform -"
         echo "d = For Division"
         echo "e = For Percentage"
 echo -n "Enter your choice : "
-read option
-case $option in
+read $1
+case $1 in
 	a)addition;;
 	b)subtract;;
 	c)multiply;;
