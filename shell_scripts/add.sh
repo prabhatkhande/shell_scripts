cd1db3a5bc9c5838defc61efc546c119963108b0
Prabhat Khande <prabhatkhande786@gmail.com>
2024-06-27 10:56:52 +0530

    added changes

---
+++ shell_scripts/add.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+num1=$1
+num2=$2
+result=$((num1 + num2))
+echo "Sum of two numbers is : $result"
--- shell_scripts/calculator.sh
+++ shell_scripts/calculator.sh
@@ -3,74 +3,44 @@
 #Simple Calculator Program
 
 function addition() {
-	echo -n "Enter first number : "
-	read num1
-	echo -n "Enter second number : "
-	read num2
-	result=$((num1 + num2))	
-	read $3
-	result=$(("$2" + "$3"))	
+	num1=$1
+	num2=$2
+	result=$((num1 + num2))		
 	echo "Sum of two numbers is : $result"
 
 }
 
 function subtract() {
-	echo -n "Enter first number : "
-        read num1
-        echo -n "Enter second number : "
-        read num2
+        num1=$1
+        num2=$2
         result=$((num1 - num2))
-        read $3
-        result=$(("$2" - "$3"))
         echo "Subtraction of two numbers is : $result"
+
 }
 
 function multiply() {
-        echo -n "Enter first number : "
-        read num1
-        echo -n "Enter second number : "
-        read num2
+        num1=$1
+        num2=$2
         result=$((num1 * num2))
-        read $3
-        result=$(("$2" * "$3"))
         echo "Multiplication of two numbers is : $result"
+
 }
 
-function division() {
-        echo -n "Enter first number : "
-        read num1
-        echo -n "Enter second number : "
-        read num2
+function divide() {
+        num1=$1
+        num2=$2
         result=$((num1 / num2))
-        read $3
-        result=$(("$2" / "$3"))
         echo "Division of two numbers is : $result"
+
 }
 
-function percent() {
-        echo -n "Enter obtained marks : "
-        read num1
-        echo -n "Enter total marks : "
-        read num2
-        result=$((100*num1 / num2))
-        read $3
-        result=$((100*"$2" / "$3"))
-        echo "Percentage of given number is : $result"
-}
+operation=$1
+num1=$2
+num2=$3
 
-
-echo "Which operation do you want to perform -"
-	echo "a = For Addition"
-        echo "b = For Subtraction"
-        echo "c = For Multiplication"
-        echo "d = For Division"
-        echo "e = For Percentage"
-echo -n "Enter your choice : "
-read choice
-case $choice in
-	a)addition;;
-	b)subtract;;
-	c)multiply;;
-	d)division;;
-	e)percent;;
+case $operation in
+	addition) addition $num1 $num2;;
+	subtract) subtract $num1 $num2;;
+	multiply) multiply $num1 $num2;;
+	divide) divide $num1 $num2;;
 esac
